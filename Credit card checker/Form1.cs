427a3434cc216bea6583cfdbf71b3f200efe0378
Daniel Seavers <daniel.p.seavers@gmail.com>
2020-06-15 13:18:20 +0100

    Add project files.

---
+++ Credit card checker/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Credit_card_checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void BTN_Validate_Click(object sender, EventArgs e)
+        {
+            string number = TBX_CardNum.Text;
+            int sum = 0;
+            bool is2 = true;
+            for(int i = number.Length-2; i > 0; i--)
+            {
+                if (is2 == true)
+                {
+                    if (int.Parse(number[i].ToString()) * 2 < 10)
+                    {
+                        sum += int.Parse(number[i].ToString()) * 2;
+                    }
+                    else
+                    {
+                        sum+= int.Parse((int.Parse(number[i].ToString()) * 2).ToString()[0].ToString()) + int.Parse((int.Parse(number[i].ToString()) * 2).ToString()[1].ToString());
+                    }
+                    is2 = false;
+                }
+                else
+                {
+                    sum += int.Parse(number[i].ToString());
+                    is2 = true;
+                }
+            }
+            int checkdigit = number[number.Length-1];
+            if ((checkdigit+sum)%10 == 0)
+            {
+                MessageBox.Show("Your card number is valid.");
+            }
+            else
+            {
+                MessageBox.Show("Your card number is not valid.");
+            }
+        }
+    }
+}
